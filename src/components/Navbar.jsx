aaaa993de01c12e72e654bbd3481033b6444459c
Archana603 <archanaharidas09@gmail.com>
2025-10-30 07:11:25 +0000

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "../style.css";
+import "../mediaqueries.css";
+
+function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      {/* Desktop Nav */}
+      <nav id="desktop-nav">
+        <div className="logo">Archana Haridas</div>
+        <div>
+          <ul className="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* Mobile Nav */}
+      <nav id="hamburger-nav">
+        <div className="logo">John Doe</div>
+        <div className="hamburger-menu">
+          <div
+            className={`hamburger-icon ${isMenuOpen ? "open" : ""}`}
+            onClick={toggleMenu}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={`menu-links ${isMenuOpen ? "open" : ""}`}>
+            <li><a href="#about" onClick={toggleMenu}>About</a></li>
+            <li><a href="#experience" onClick={toggleMenu}>Experience</a></li>
+            <li><a href="#projects" onClick={toggleMenu}>Projects</a></li>
+            <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
